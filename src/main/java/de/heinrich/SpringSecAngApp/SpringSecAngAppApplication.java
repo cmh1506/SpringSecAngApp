51b5946dbf04f0fdee6458930099ffb0cc87a1a0
cmh1506 <cmh1506@yahoo.de>
2021-11-14 20:20:42 +0100

    Initial Commit

---
+++ src/main/java/de/heinrich/SpringSecAngApp/SpringSecAngAppApplication.java
@@ -0,0 +1,13 @@
+package de.heinrich.SpringSecAngApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecAngAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecAngAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/heinrich/SpringSecAngApp/SpringSecAngAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.heinrich.SpringSecAngApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecAngAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
